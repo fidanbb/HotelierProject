e79f05451f7f2f5c2604eb1417b8cd45b235ca20
fidanbb <fidanbb@code.edu.az>
2024-06-21 01:13:49 +0400

    checked jwtToken

---
+++ JwtProject/WebApiJwt/Controllers/DefaultController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiJwt.Models;
+
+namespace WebApiJwt.Controllers
+{
+    [Route("api/[controller]")]
+    
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult CreateToken()
+        {
+            return Ok(new TokenGenerator().TokenCreate());
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult AdminCreateToken()
+        {
+            return Ok(new TokenGenerator().TokenCreateAdmin());
+        }
+
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        [HttpGet("[action]")]
+        public IActionResult Test2()
+        {
+            return Ok("Welcome");
+        }
+        [Authorize(AuthenticationSchemes = "Bearer",Roles = "Admin,Visitor")]
+       // [Authorize(Roles = "Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult Test3()
+        {
+            return Ok("Token Login Successfully");
+        }
+    }
+}
