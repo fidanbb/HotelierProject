01d6d159d539a9b8d34fa1c0ddc40a93677752ec
fidanbb <fidanbb@code.edu.az>
2024-06-22 21:09:16 +0400

    added mail sending codes

---
+++ Frontend/HotelierProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,42 @@
+﻿using HotelierProject.WebUI.Dtos.MailDtos;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace HotelierProject.WebUI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AdminMailDto model)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("HotelierAdmin", "bashirova.fidangs@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = model.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = model.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("bashirova.fidangs@gmail.com", "rmnt savk gcat thst");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+            
+            return View();
+        }
+    }
+}
